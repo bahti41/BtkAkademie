9f0ad1073e32b77632298198a986d4b7cabc40e7
bahti41 <sonmezbahtiyar4@gmail.com>
2024-05-16 01:56:30 +0300

    devammm

--- BtkAkademie/Entities/LinkModels/Link.cs
+++ BtkAkademie/Entities/LinkModels/Link.cs
@@ -9,7 +9,7 @@ namespace Entities.LinkModels
     public class Link
     {
         public string? Href { get; set; }
-        public string? Ref { get; set; }
+        public string? Rel { get; set; }
         public string? Method { get; set; }
         
         public Link() 
@@ -17,11 +17,21 @@ namespace Entities.LinkModels
 
         }
 
-        public Link(string? href, string? @ref, string? method)
+        public Link(string? href, string? rel, string? method)
         {
             Href = href;
-            Ref = @ref;
+            Rel = rel;
             Method = method;
         }
+    }
+
+    public class LinkResourceBase
+    {
+        public LinkResourceBase()
+        {
+            
+        }
+
+        public List<Link> Links { get; set; } = new List<Link>();
     }
 }
