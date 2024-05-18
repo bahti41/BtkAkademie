60ca788faa8f707ab583e4b440831ed2a3e1bfb3
bahti41 <sonmezbahtiyar4@gmail.com>
2024-05-18 18:43:39 +0300

    Service katmanı is okey Controller'a register

---
+++ BtkAkademie/Services/AuthenticationManager.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AuthenticationManager : IAuthenticationService
+    {
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationManager(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegisterDTO userForRegisterDTO)
+        {
+            var user = _mapper.Map<User>(userForRegisterDTO);
+
+            var result = await _userManager.CreateAsync(user,userForRegisterDTO.Password);
+
+            if (result.Succeeded)
+                await _userManager.AddToRolesAsync(user, userForRegisterDTO.Roles);
+            return result;
+        }
+    }
+}
--- BtkAkademie/Services/ServiceManager.cs
+++ BtkAkademie/Services/ServiceManager.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using Entities.Concrete;
 using Entities.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Repositories.Contracts;
 using Services.Contracts;
 using System;
@@ -13,10 +16,22 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
-        public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper,IBookLinks bookLinks)
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+
+        public ServiceManager(
+            IRepositoryManager repositoryManager, 
+            ILoggerService logger,
+            IConfiguration configuration, 
+            UserManager<User> userManager,
+            IMapper mapper,
+            IBookLinks bookLinks)
         {
             _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper,bookLinks));
+
+            _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationManager(logger,mapper,userManager,configuration));
         }
         public IBookService BookService => _bookService.Value;
+
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
     }
 }
